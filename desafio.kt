54a98732afd558fe2eb347c3ca087d2f4c49b21c
joaobasilio77 <146891284+joaobasilio77@users.noreply.github.com>
2023-10-14 23:17:03 -0300

    joaobasilio77_codigokt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,90 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String, val email: String) {
+    val formacoesInscritas = mutableListOf<Formacao>()
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+    fun matricularNaFormacao(formacao: Formacao) {
+        if (formacao !in formacoesInscritas) {
+            formacoesInscritas.add(formacao)
+            formacao.matricular(this)
+        }
+    }
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+    fun visualizarProgresso(formacao: Formacao): Double {
+        val totalAulas = formacao.conteudos.size
+        if (totalAulas == 0) {
+            return 0.0 
+        }
+        val aulasConcluidas = formacao.aulasConcluidas[this] ?: 0
+        return (aulasConcluidas.toDouble() / totalAulas) * 100
+    }
 
+    fun marcarAulaConcluida(formacao: Formacao, conteudo: ConteudoEducacional) {
+        formacao.marcarAulaConcluida(this, conteudo)
+    }
+}
+
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60, val nivel: Nivel)
+
+class Formacao(val nome: String, val nivel: Nivel) {
+    val conteudos = mutableListOf<ConteudoEducacional>()
     val inscritos = mutableListOf<Usuario>()
-    
+    val aulasConcluidas = mutableMapOf<Usuario, Int>()
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        aulasConcluidas[usuario] = 0
+        usuario.formacoesInscritas.add(this)
+    }
+
+    fun listarInscritos(): List<Usuario> {
+        return inscritos
+    }
+
+    fun adicionarConteudo(conteudo: ConteudoEducacional) {
+        conteudos.add(conteudo)
+    }
+
+    fun marcarAulaConcluida(usuario: Usuario, conteudo: ConteudoEducacional) {
+        if (conteudo in conteudos) {
+            aulasConcluidas[usuario] = (aulasConcluidas[usuario] ?: 0) + 1
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 90, Nivel.BASICO)
+    val conteudo2 = ConteudoEducacional("Classes e Objetos em Kotlin", nivel = Nivel.INTERMEDIARIO)
+    val conteudo3 = ConteudoEducacional("Desenvolvimento mobile em Kotlin", nivel = Nivel.INTERMEDIARIO)
+
+    val formacaoKotlin = Formacao("Formação Kotlin", Nivel.INTERMEDIARIO)
+
+    val usuario1 = Usuario("JoãoEnterprises", "jao@email.com")
+    val usuario2 = Usuario("Leticie", "letixeee@email.com")
+
+    formacaoKotlin.matricular(usuario1)
+    formacaoKotlin.matricular(usuario2)
+
+    usuario1.matricularNaFormacao(formacaoKotlin)
+
+    usuario1.marcarAulaConcluida(formacaoKotlin, conteudo1)
+    usuario1.marcarAulaConcluida(formacaoKotlin, conteudo2)
+
+    println("Usuários inscritos na formação Kotlin: ${formacaoKotlin.listarInscritos().map { it.nome }}")
+    
+    val progressoAlice = usuario1.visualizarProgresso(formacaoKotlin)
+    if (progressoAlice.isNaN()) {
+        println("Progresso de JoãoEnterprises na formação Kotlin: 0.0%")
+    } else {
+        println("Progresso de JoãoEnterprises na formação Kotlin: ${progressoAlice}%")
+    }
+
+    formacaoKotlin.adicionarConteudo(conteudo3)
+
+    println("Conteúdos de nível INTERMEDIARIO na formação Kotlin: ${formacaoKotlin.conteudos.filter { it.nivel == Nivel.INTERMEDIARIO }.map { it.nome }}")
 }
+
+
+
+
+
